36127df36477a33cba3ba83e0792a4b46d683c5f
Admin <sohilasobhysadek@gmail.com>
2025-02-18 12:10:47 +0200

    Edits 18/2/2025

--- Index.js
+++ Index.js
@@ -6,20 +6,20 @@ document.getElementById('closeMenu').addEventListener('click', function () {
     const menu = document.getElementById('navbarSupportedContent');
     menu.classList.remove('show');
 });
-document.addEventListener("DOMContentLoaded", function () {
-    const carousel = document.querySelector("#carouselExampleIndicators");
-    const prevButton = carousel.querySelector(".carousel-control-prev");
-    const nextButton = carousel.querySelector(".carousel-control-next");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const carousel = document.querySelector("#carouselExampleIndicators");
+//     const prevButton = carousel.querySelector(".carousel-control-prev");
+//     const nextButton = carousel.querySelector(".carousel-control-next");
 
-    function updateButtonStyles() {
-        const activeItem = carousel.querySelector(".carousel-item.active");
-        const allItems = [...carousel.querySelectorAll(".carousel-item")];
-        const activeIndex = allItems.indexOf(activeItem);
+//     function updateButtonStyles() {
+//         const activeItem = carousel.querySelector(".carousel-item.active");
+//         const allItems = [...carousel.querySelectorAll(".carousel-item")];
+//         const activeIndex = allItems.indexOf(activeItem);
 
-        prevButton.classList.toggle("disabled", activeIndex === 0);
-        nextButton.classList.toggle("disabled", activeIndex === allItems.length - 1);
-    }
+//         prevButton.classList.toggle("disabled", activeIndex === 0);
+//         nextButton.classList.toggle("disabled", activeIndex === allItems.length - 1);
+//     }
 
-    carousel.addEventListener("slid.bs.carousel", updateButtonStyles);
-    updateButtonStyles();
-});
+//     carousel.addEventListener("slid.bs.carousel", updateButtonStyles);
+//     updateButtonStyles();
+// });
